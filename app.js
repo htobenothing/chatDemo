52ac08e08b22867f78534833d7ba553fdfa0d5f1
Nothing <nothing@nothing.local>
2017-08-23 01:34:42 +0800

    add video connect

--- app.js
+++ app.js
@@ -16,46 +16,48 @@ app.use(express.static(__dirname + "/public"))
 // connection will call first
 io.on('connection', (socket) => {
 
+    socket.on('createOrJoin', (room) => {
 
-    socket.on('newUser', (name) => {
-        clientInfo = {
-            name: name,
-            id: socket.id,
-            createDate: new Date(),
-        }
-
-        storeClientInfo(clientInfo)
-        socket.broadcast.emit('welcome',name + " is join the room ,let's welcome ")
-        let welcome = 'Hello ' + name + '! Welcome to join the chat room, there is ' + clients.length + " Clients in the room"
-        socket.emit('welcome', welcome)
-    })
-
-
-    socket.on('disconnect', () => {
-
-        let client = removeClientInfo(socket.id)
-        let message = client[0].name + " left the room"
-        socket.broadcast.emit('bye', message)
-    })
-
-    socket.on('message', (msg) => {
-        console.log("get message from client:", msg)
-        socket.broadcast.emit("message", msg)
-    })
-
-    socket.on('pmsg', (resp) => {
-        let youName = resp.youName
-        let tarName = resp.tarName
-        let msg = resp.message
-    
-        let id =  getSocketIDByName(tarName)
-        if(id !==undefined){
-            socket.broadcast.to(id).emit('message',youName+":"+msg)
+        let numClients;
+        let socketRoom = io.sockets.adapter.rooms[room]
+        if(socketRoom!==undefined){
+            numClients = socketRoom.length
+        }else{
+            numClients=0
         }
         
+     
+        if (numClients===0) {
+            console.log("create", room)
+            socket.join(room)
+            socket.emit('create', room)
+        } else {
+            if (numClients < 5) {
+                console.log(" join room",room)
+                io.sockets.in(room).emit('join', room)
+                socket.join(room)
+                socket.emit('joined', room)
+            } else {
+                socket.emit('full', room)
+            }
+        }
+
+
     })
 
+    socket.on("message", (message) => {
+        log('S --> got message: ', message);
+        console.log("channel",message.channel)
+        socket.broadcast.to('room1').emit('message', message)
+    })
 
+    function log() {
+        var array = [">>> "];
+        for (var i = 0; i < arguments.length; i++) {
+            array.push(arguments[i]);
+        }
+        socket.emit('log', array);
+    }
 
 })
 
@@ -86,10 +88,10 @@ function getSocketIDByName(name) {
 
 
 
-    if(index > -1){
+    if (index > -1) {
         return clients[index].id
     }
-    
+
 }
 
 function removeClientInfo(id) {
